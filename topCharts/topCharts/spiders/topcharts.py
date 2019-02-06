8dda97899d040c1704d4846cb09cad50f6c790e2
Matt LaBarre <44075733+mattlabarre@users.noreply.github.com>
2019-02-06 09:26:40 -0700

    Add files via upload

--- topCharts/topCharts/spiders/topcharts.py
+++ topCharts/topCharts/spiders/topcharts.py
@@ -8,11 +8,15 @@ class TopchartsSpider(scrapy.Spider):
     start_urls = ['https://www.at40.com/charts/top-40-238/latest/']
 
     def parse(self, response):
+        #Pulls all of the containers that have the song and artist in them
         songs = response.xpath('//div[@class="chart-item-info"]/h3')
         for song in songs:
+            #Pulls the individual songs from the containers
             title = song.xpath('./a/text()').extract_first()
+            #Pulls the individual artists from the containers
             artist = song.xpath('.//small/a/text()').extract_first()
 
+            #Used to output all of the songs and artists that are yielded
             yield {
                 'Song':title,
                 'Artist':artist,
